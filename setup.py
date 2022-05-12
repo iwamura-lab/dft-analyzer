faf52bc73bb1bc65f391412ffa6c008b5ae8abf4
Taiki Iwamura <takki.0206@gmail.com>
2022-05-12 20:24:31 +0900

    Add setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dft_analyzer",
+    version="1.1.0",
+    author="Taiki Iwamura",
+    author_email="takki.0206@gmail.com",
+    description="Analyzer for dft calculation to compare with machine learning potential calculation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwamura-lab/dft-analyzer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">= 3.7",
+    install_requires=[
+        "pymatgen",
+    ],
+)
