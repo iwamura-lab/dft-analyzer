56d84dad5ec01b72ebdf8624faa88522bb813ee7
Taiki Iwamura <takki.0206@gmail.com>
2022-06-30 11:46:57 +0900

    Add eos_tools/config.py

---
+++ eos_tools/config.py
@@ -0,0 +1,26 @@
+import json
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class Config:
+    mode: str
+    inputs_dir: str
+    eos_name: str
+
+
+def load_config(path: str) -> Config:
+    """Load configs/*.json
+
+    Args:
+        path (str): path to configs/*.json
+
+    Returns:
+        Config: EOS calculation config dataclass
+    """
+    with open(path) as f:
+        config_dict = json.load(f)
+    return Config.from_dict(config_dict)  # type: ignore
