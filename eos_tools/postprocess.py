e9304240a64fbb59a9866ca72b53f4d747ddc48a
Taiki Iwamura <takki.0206@gmail.com>
2023-10-04 10:26:44 +0900

    Change variable names

--- eos_tools/postprocess.py
+++ eos_tools/postprocess.py
@@ -9,20 +9,20 @@ from pymatgen.io.vasp import Vasprun
 logger = logging.getLogger(__name__)
 
 
-def parse_volume_and_energy(dir_path: Path) -> Tuple[float, float]:
-    """Parse volume and total energy from a given directory
+def parse_volume_and_energy(calc_dir_path: Path) -> Tuple[float, float]:
+    """Parse volume and total energy from calculation directory
 
     Args:
-        dir_path (Path): Path object of calculation directory
+        calc_dir_path (Path): Path object of calculation directory
 
     Returns:
         Tuple[float, float]: volume and total energy
     """
-    logger.info(f" Analysing {dir_path.stem}")
+    logger.info(f" Analysing {calc_dir_path.name}")
 
     # Extract lattice constant from directory name
     lattice_constant_pattern = re.compile(r"\d+")
-    m = lattice_constant_pattern.search(dir_path.stem)
+    m = lattice_constant_pattern.search(calc_dir_path.name)
     lattice_constant = float(m.group(0)) / 100
     volume = lattice_constant**3
 
@@ -30,12 +30,12 @@ def parse_volume_and_energy(dir_path: Path) -> Tuple[float, float]:
     logger.info(f"      volume (ang^3)        : {volume}")
 
     # Extract total energy from vasprun_xml.json
-    vasprun_xml_json_path = dir_path / "vasprun_xml.json"
+    vasprun_xml_json_path = calc_dir_path / "vasprun_xml.json"
     if vasprun_xml_json_path.exists():
         with vasprun_xml_json_path.open("r") as f:
             vasprun_dict = json.load(f)
     else:
-        vasprun_xml_path = dir_path / "vasprun.xml"
+        vasprun_xml_path = calc_dir_path / "vasprun.xml"
         vasprun = Vasprun(str(vasprun_xml_path), parse_potcar_file=False)
         vasprun_dict = vasprun.as_dict()
         with vasprun_xml_json_path.open("w") as f:
